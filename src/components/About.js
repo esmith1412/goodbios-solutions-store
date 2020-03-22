3438685b41b2cf9626b6c47002980ccdae026f0a
Elijah Smith <esmith1412@posteo.net>
2020-03-22 12:15:41 -0400

    Updated About Us page, to include links to my eBay listings, for the same service

--- src/components/About.js
+++ src/components/About.js
@@ -12,6 +12,8 @@ export default function About() {
 
       <p>After looking at the market, and months of research, we developed a way to turn a profit, without having to have prices beyond the reach of the average American; this was when we started GoodBIOS Solutions.</p>
 
+      <p>We have been on eBay for a while, offering our services there (see our <a href="https://www.ebay.com/itm/283663157808">SOIC8 and SOIC16 service</a>, and our <a href="https://www.ebay.com/itm/283578583351">WSON8 service</a>), but between the fees and flawed messaging system, we felt that it was time to move onto our own platform, and that's where this site comes in: simple, but effective.</p>
+
       <p>The process for using our service is very simple: You add as many installations as you need to your cart; pay through Paypal, and then send an email to the administrator with your Paypal order number, device(s) you'll be sending, and any special requests.* Please make sure to ask any questions you have prior to making a purchase, as refunds will only be offered for errors that occur on our end.</p>
 
       <p>From there you will receive an email from the adminstrator with instructions for shipping your device(s). Once we receive the device(s), depending on order volume, of course, the service will be completed in 1-2 business days (3-4 at high-volume times), and then shipped back to you, using Medium Flat-Rate 2-Day USPS shipping.</p>
